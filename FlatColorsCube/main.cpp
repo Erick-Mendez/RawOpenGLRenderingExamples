a1008d54dc435fda015ef4d0d175486181ba5a1d
Erick Mendez <erick.mendez@daqri.com>
2016-11-28 22:20:34 +0100

    Initial commit. Working verion on Ubuntu 16 and macOS Sierra

---
+++ FlatColorsCube/main.cpp
@@ -0,0 +1,238 @@
+/*
+ The MIT License (MIT)
+ Copyright (c) 2016 Erick Mendez
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#define GLEW_STATIC
+#ifdef __APPLE__
+#   include <GL/glew.h>
+#   include <OpenGL/gl.h>
+#   include <OpenGL/glext.h>
+#else
+#   include <GL/glew.h>
+#   include <GL/gl.h>
+#   include <GL/glext.h>
+#endif
+
+#include <iostream>
+#include <SDL2/SDL.h>
+
+/// These are the variables used for OpenGL
+/// structures, buffers, arrays, etc.
+GLuint arrayBufferVertexPositions = 0;
+GLuint vertexArrayCube = 0;
+GLuint elementBufferIndices = 0;
+GLuint shaderProgram = 0;
+GLint uniLocProjectionMatrix = 0;
+GLint uniLocModelViewMatrix = 0;
+GLint attribLocVertexPosition = 0;
+
+/// The Shader source code
+/// The escaped new line on the #version lines is needed on Linux
+const GLchar* vertexShaderSource ="\
+#version 150 core\n \
+uniform mat4 projectionMatrix; \
+uniform mat4 modelviewMatrix; \
+in vec3 vertexPosition; \
+void main() { \
+    gl_Position = projectionMatrix * modelviewMatrix  * vec4(vertexPosition, 1.0); \
+}";
+
+const GLchar* fragmentShaderSource ="\
+#version 150 core\n \
+out vec4 outColor; \
+void main() { \
+    outColor = vec4(1.0, 0.0, 0.0, 1.0); \
+}";
+
+
+/// Here is the geometry of the cube including vertices, colors and indices
+/// as well as a hard coded modelview projection matrix
+const GLfloat projectionMatrix[16] = { 4.48182, 0, 0, 0, 0, 5.97576, 0, 0, 0, 0, -1.0002, -1, 0, 0,-0.20002, 0};
+const GLfloat modelviewMatrix[16] = { -0.707107, -0.372046, 0.601317, 0, 0, 0.85039, 0.526152, 0, -0.707107, 0.372046, -0.601317, 0, 0,4.76837e-07, -13.3041, 1 };
+
+/// Vertices of the cube. Each row is a triangle. Each pair of rows is a face.
+/// 6 faces x 2 triangles (per face) x 3 vertices (per triangle)  x 3 values (per vertex)= 108 floats
+const size_t numDataVertexPositions = 6*2*3*3;
+const GLfloat dataVertexPositions[] = {
+    // clang-format off
+    -1.00f, -1.00f, -1.00f,     +1.00f, -1.00f, -1.00f,     +1.00f, +1.00f, -1.00f,
+    +1.00f, +1.00f, -1.00f,     -1.00f, +1.00f, -1.00f,     -1.00f, -1.00f, -1.00f,
+    
+    +1.00f, -1.00f, -1.00f,     +1.00f, -1.00f, +1.00f,     +1.00f, +1.00f, +1.00f,
+    +1.00f, +1.00f, +1.00f,     +1.00f, +1.00f, -1.00f,     +1.00f, -1.00f, -1.00f,
+    
+    +1.00f, -1.00f, +1.00f,     -1.00f, -1.00f, +1.00f,     -1.00f, +1.00f, +1.00f,
+    -1.00f, +1.00f, +1.00f,     +1.00f, +1.00f, +1.00f,     +1.00f, -1.00f, +1.00f,
+    
+    -1.00f, -1.00f, +1.00f,     -1.00f, -1.00f, -1.00f,     -1.00f, +1.00f, -1.00f,
+    -1.00f, +1.00f, -1.00f,     -1.00f, +1.00f, +1.00f,     -1.00f, -1.00f, +1.00f,
+    
+    -1.00f, +1.00f, -1.00f,     +1.00f, +1.00f, -1.00f,     +1.00f, +1.00f, +1.00f,
+    +1.00f, +1.00f, +1.00f,     -1.00f, +1.00f, +1.00f,     -1.00f, +1.00f, -1.00f,
+    
+    -1.00f, -1.00f, +1.00f,     +1.00f, -1.00f, +1.00f,     +1.00f, -1.00f, -1.00f,
+    +1.00f, -1.00f, -1.00f,     -1.00f, -1.00f, -1.00f,     -1.00f, -1.00f, +1.00f
+    // clang-format on
+};
+
+/// There are two triangles for each of the 6 faces
+const size_t numDataIndices = 6*2*3*1;
+const GLuint dataIndices[] = {
+    // clang-format off
+    0,  1,  2,      3,  4,  5,
+    6,  7,  8,      9, 10, 11,
+    12, 13, 14,     15, 16, 17,
+    18, 19, 20,     21, 22, 23,
+    24, 25, 26,     27, 28, 29,
+    30, 31, 32,     33, 34, 35
+    // clang-format on
+};
+
+/// Simple error checking function, only prints the error
+void CheckForGLErrors(const char* msg)
+{
+    GLenum glErr = glGetError();
+    if (glErr != GL_NO_ERROR)
+        std::cout << "glError [" << msg << "]: " << glErr << "\n";
+}
+
+int main()
+{
+    /// This section creates the window
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to init SDL\n";
+        return -1;
+    }
+    
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    
+    SDL_Window *sdl_Window = SDL_CreateWindow("Flat Colors Cube",
+                                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                              640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    
+    if (sdl_Window == NULL)
+    {
+        std::cout << "Unable to create window\n";
+        return -1;
+    }
+    
+    SDL_GLContext sdl_Context = SDL_GL_CreateContext(sdl_Window);
+    SDL_GL_MakeCurrent(sdl_Window, sdl_Context);
+    
+    /// At this point the window has been created
+    /// We now initialize Glew and set some basic OpenGL settings
+    glewExperimental = GL_TRUE; // Needed on Linux, harmless on macOS
+    if(glewInit()!=GLEW_OK)
+    {
+        std::cout << "Glew failed \n";
+        return -1;
+    }
+    CheckForGLErrors("After glew");
+    
+    /// Clear color is a tad blue, useful for debugging
+    /// if it shows full black then something went wrong with the context or the window
+    glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+    CheckForGLErrors("After basic GL state settings");
+    
+    /// We now create all the buffers and arrays
+    glGenVertexArrays(1, &vertexArrayCube);
+    glBindVertexArray(vertexArrayCube);
+    CheckForGLErrors("After creating the vertex array where the cube is");
+    
+    glGenBuffers(1, &arrayBufferVertexPositions);
+    glBindBuffer(GL_ARRAY_BUFFER, arrayBufferVertexPositions);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*numDataVertexPositions, dataVertexPositions, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    CheckForGLErrors("Creating the Vertex Buffer of the cube");
+    
+    glGenBuffers(1, &elementBufferIndices);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferIndices);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*numDataIndices, dataIndices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    CheckForGLErrors("Creating the indices buffer of the cube");
+    
+    /// We now create our shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    CheckForGLErrors("Compiling shaders");
+    
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+    CheckForGLErrors("Linking shaders and creating the program");
+    
+    /// Get the attributes and uniforms locations
+    uniLocProjectionMatrix = glGetUniformLocation(shaderProgram, "projectionMatrix");
+    uniLocModelViewMatrix = glGetUniformLocation(shaderProgram, "modelviewMatrix");
+    attribLocVertexPosition = glGetAttribLocation(shaderProgram, "vertexPosition");
+    CheckForGLErrors("Getting the uniform and attibutes locations");
+    glUseProgram(0);
+    
+    /// We are ready to do our render loop
+    bool leaveASAP = false;
+    while(!leaveASAP)
+    {
+        /// Very simple check to see if we need to close the window
+        SDL_Event sdl_Event;
+        while (SDL_PollEvent(&sdl_Event))
+        {
+            if (sdl_Event.type == SDL_QUIT)
+                leaveASAP = true;
+            else if (sdl_Event.type == SDL_KEYUP && sdl_Event.key.keysym.sym == SDLK_ESCAPE)
+                leaveASAP = true;
+            else if (sdl_Event.type == SDL_WINDOWEVENT && sdl_Event.window.event == SDL_WINDOWEVENT_CLOSE)
+                leaveASAP = true;
+        }
+        
+        glUseProgram(shaderProgram);
+        /// This is the actual display block
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUniformMatrix4fv(uniLocProjectionMatrix, 1, GL_FALSE, &projectionMatrix[0]);
+        glUniformMatrix4fv(uniLocModelViewMatrix, 1, GL_FALSE, &modelviewMatrix[0]);
+
+        glBindBuffer(GL_ARRAY_BUFFER, arrayBufferVertexPositions);
+        glEnableVertexAttribArray(attribLocVertexPosition);
+        glVertexAttribPointer(attribLocVertexPosition, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+        
+        // Comment or uncomment any of the two lines below to see either arrays or elements drawing
+        glDrawArrays(GL_TRIANGLES, 0, numDataIndices);
+//        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferIndices);
+//        glDrawElements(GL_TRIANGLES, numDataIndices, GL_UNSIGNED_INT, NULL);
+        
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDisableVertexAttribArray(attribLocVertexPosition);
+        glUseProgram(0);
+        CheckForGLErrors("Drawing");
+        SDL_GL_SwapWindow(sdl_Window);
+    }
+    
+    SDL_GL_DeleteContext(sdl_Context);
+    SDL_DestroyWindow(sdl_Window);
+}
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# RawOpenGLRenderingExamples
-A set of single-file examples of OpenGL functionality
+# "Raw" OpenGL Rendering Examples
+
+This repository shows a set of minimal examples of how to do rendering with OpenGL. Most examples use a hard coded cube with a hard coded pose and projection. The target audience of this repo are developers who are familiar with OpenGL and just want to get a minimal version of what they already know how to write, but dont have the time or patience to type again. I call them "raw" because of the lack of dependencies or internally created libraries.
+
+## Goals of this repo
+- There are no external libraries needed except one for window management (currently SDL but trivially exchangeable to others, for example glfw)
+- Every example is a *single .cpp file*
+- Every example is self contained and does not depend on previous examples
+- There are no classes that abstract away common functionalities (like loading shaders)
+- There are no external header files that hide away nuisances
+- There is a minimal set of custom functions, most is contained within the main function
+- Hard coded geometry, no loading of OBJ files, textures or similar
+- Comments only when necessary and highlighting larger logical blocks
+- Verbose variable names
+- It builds on *multiple platforms*, currently targeting Ubuntu Linux, macOS and Windows 10
+- It builds with multiple compilers, currently targeting gcc, clang, msbuild
+- Built with cmake
+- Minimal usage of ifdefs
+- No dependencies to the STL
+- No command line parameters needed
+- *Minimal error checking*. Yes, you read that right, minimal error checking. The code in here should be correct but will not be very resistance to things going severely wrong (like running out of memory). Users of this library know that a lot of more error checking is needed, for example, the result of shader compilation, but just want to see the bare minimum in a working state
+
+## What you will *not* get from this repo:
+- Interaction examples. There are no examples that will show how to rotate geometry or change the viewpoint
+- Math examples. For example, how to generate a projection matrix or how to compute the model view matrix
+- Libraries or a Framework. These are "Raw examples"
+- Reusable code among the examples
+- Mobile examples. Officially this code is not checked with Android or iOS, but it should be trivial to port it
+- Exhaustive checking. I cant stress this enough, you need to take care of error checking yourself
+
+## Building
+The repo is built using cmake. Make sure you have SDL2 and Cmake installed. General steps for building on any platform are:
+- Create a 'build' folder
+- Go inside the folder
+- Run `cmake ..`
+- Your project files have been generated, you can now build as usual
+
+## Examples
+These are the current examples:
+
+|    Example     | Shows                                    | How it should look like                |
+|----------------|------------------------------------------|----------------------------------------|
+|FlatColorsCube  | A cube with one flat color |![FlatColorsCube][FlatColorsCubeImg]    |
+|VertexColorsCube| A cube with vertex coloring|![VertexColorsCube][VertexColorsCubeImg]|
+|TexturedCube    | A cube with texturing      |![TexturedCube][TexturedCubeImg]|
+
+
+[FlatColorsCubeImg]: ./images/FlatColorsCube.png "FlatColorsCube"
+[VertexColorsCubeImg]: ./images/VertexColorsCube.png "VertexColorsCube"
+[TexturedCubeImg]: ./images/TexturedCube.png "TexturedCube"
